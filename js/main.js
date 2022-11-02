7131a456eb686cdd58ad9953913c581c56a30974
ManganiBiagio <biagiomangani@live.it>
2022-11-02 19:41:17 +0100

    correzione codice if

--- js/main.js
+++ js/main.js
@@ -1,27 +1,69 @@
-const boxSquareEl=document.querySelector(".box-quadrati");
-console.log(boxSquareEl);
+const boxSquareEl = document.querySelector(".box-quadrati");
 
-for(let i=1;i<=100;i++){
-    
-    if(i % 3===0){
-        if(i % 5===0){
-            console.log("FizzBuzz");
-            boxSquareEl.innerHTML += `<div class="my-square red"> FizzBuzz </div>`
-        }
-        else{
-            console.log("Fizz");
+
+for (let i = 1; i <= 100; i++) {
+    if((i % 3===0) && (i % 5===0)){
+        console.log("FizzBuzz");
+        boxSquareEl.innerHTML += `<div class="my-square red"> FizzBuzz </div>`;
+    }
+    else if(i % 3===0){
+        console.log("Fizz");
             boxSquareEl.innerHTML += `<div class="my-square green"> Fizz </div>`
-        }
+
     }
     else if(i % 5===0){
          console.log("Buzz");
          boxSquareEl.innerHTML += `<div class="my-square yellow"> Buzz </div>`
-        
+
     }
     else{
         console.log(i);
         boxSquareEl.innerHTML += `<div class="my-square blu"> ${i} </div>`
     }
 
+    // if(i % 3===0){
+    //     if(i % 5===0){
+    //         console.log("FizzBuzz");
+    //         boxSquareEl.innerHTML += `<div class="my-square red"> FizzBuzz </div>`
+    //     }
+    //     else{
+    //         console.log("Fizz");
+    //         boxSquareEl.innerHTML += `<div class="my-square green"> Fizz </div>`
+    //     }
+    // }
+    // else if(i % 5===0){
+    //      console.log("Buzz");
+    //      boxSquareEl.innerHTML += `<div class="my-square yellow"> Buzz </div>`
+
+    // }
+    // else{
+    //     console.log(i);
+    //     boxSquareEl.innerHTML += `<div class="my-square blu"> ${i} </div>`
+    // }
+
+
+    // ----------------------NON FUNZIONA----------------------------
+    // switch (i) {
+        
+    //     case (i % 3 ===0) :
+    //         console.log("Fizz");
+    //         boxSquareEl.innerHTML += `<div class="my-square green"> Fizz </div>`;
+    //         break;
+    //     case (i % 5 === 0):
+    //         console.log("Buzz");
+    //         boxSquareEl.innerHTML += `<div class="my-square yellow"> Buzz </div>`;
+    //         break;
+    //     case ((i % 3 === 0)&&(i % 5 === 0)):
+    //         console.log("FizzBuzz");
+    //         boxSquareEl.innerHTML += `<div class="my-square red"> FizzBuzz </div>`;
+    //         break;
+    //     default:
+    //         console.log(i);
+    //         boxSquareEl.innerHTML += `<div class="my-square blu"> ${i} </div>`  ;
+            
+
+
+    // }
+    // --------------------------------------------------
 
 }
